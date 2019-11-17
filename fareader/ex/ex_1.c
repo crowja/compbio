3713d500897eea59cad20f808903c1e4d8be6abd
John Crow <crowja@gmail.com>
2019-11-17 16:44:34 -0600

    Print description and sequence text.

--- fareader/ex/ex_1.c
+++ fareader/ex/ex_1.c
@@ -5,11 +5,12 @@
 int
 main(int argc, char *argv[])
 {
-   struct fareader *r = fareader_new(NULL);
+   struct fareader *r = fareader_new(NULL); /* from stdin */
    char       *h, *s;
 
    while (fareader_next(r, &h, &s)) {
-      printf("seq is %s\n", s);
+      printf("seq desc is %s\n", h);
+      printf("seq text is %s\n", s);
    }
 
    fareader_free(r);
